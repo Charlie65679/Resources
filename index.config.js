9d39030fef483cffe85c7a5a37178905c18ad862
Charlie65679 <g3286475807@gmail.com>
2026-06-11 21:27:13 +0800

    更新 index.config.js

--- index.config.js
+++ index.config.js
@@ -73,6 +73,7 @@ var index_config_default = {
   },
 t4: {
   list: [
+    { "name": "电影", "address": "https://movie.jsnzkpg.ccwu.cc" },
     { "name": "荐片", "address": "http://zhangqun1818.serv00.net/jianpian1.php" },
     { "name": "影视爬虫", "address": "https://dy.7772888.xyz/api.php/tvbox" },
     { "name": "电影天堂", "address": "https://php.doube.eu.org/spider/php/电影天堂.php" },
